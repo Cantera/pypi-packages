f33aed90f72ab888792e5e136fdbb7ec3781eae3
Bryan Weber <bryan.w.weber@gmail.com>
2024-07-15 15:40:16 -0400

    Adapt windows builder from h5py

---
+++ cibw_before_all_win.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# Copied from h5py. Licensed under the BSD 3-Clause license.
+# Copyright (c) 2008 Andrew Collette and contributors
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the
+#    distribution.
+
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+set -eo pipefail
+
+if [[ "$1" == "" ]] ; then
+    echo "Usage: $0 <PROJECT_PATH>"
+    exit 1
+fi
+PROJECT_PATH="$1"
+
+# nuget
+nuget install zlib-msvc-x64 -ExcludeVersion -OutputDirectory "$PROJECT_PATH"
+EXTRA_PATH="$PROJECT_PATH\zlib-msvc-x64\build\native\bin_release"
+export PATH="$PATH:$EXTRA_PATH"
+export CL="/I$PROJECT_PATH\zlib-msvc-x64\build\native\include"
+export LINK="/LIBPATH:$PROJECT_PATH\zlib-msvc-x64\build\native\lib_release"
+export ZLIB_ROOT="$PROJECT_PATH\zlib-msvc-x64\build\native"
+
+# HDF5
+export HDF5_VERSION="1.14.2"
+export HDF5_VSVERSION="17-64"
+export HDF5_DIR="$PROJECT_PATH/cache/hdf5/$HDF5_VERSION"
+
+python -m pip install requests
+python $PROJECT_PATH/get_hdf5_win.py
+
+if [[ "$GITHUB_ENV" != "" ]] ; then
+    # PATH on windows is special
+    echo "$EXTRA_PATH" | tee -a $GITHUB_PATH
+    echo "CL=$CL" | tee -a $GITHUB_ENV
+    echo "LINK=$LINK" | tee -a $GITHUB_ENV
+    echo "ZLIB_ROOT=$ZLIB_ROOT" | tee -a $GITHUB_ENV
+    echo "HDF5_DIR=$HDF5_DIR" | tee -a $GITHUB_ENV
+fi
