153441b98ed60bb4b83f9186c3bbbc03c073cde2
Bryan Weber <bryan.w.weber@gmail.com>
2024-07-16 12:31:32 -0400

    Set MACOSX_DEPLOYMENT_TARGET for hdf5
    We need to set MACOSX_DEPLOYMENT_TARGET when building HDF5, explained here https://github.com/h5py/h5py/pull/2444/files#r1679541011

--- cibw_before_all_macos.sh
+++ cibw_before_all_macos.sh
@@ -42,6 +42,14 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ARCH=$(uname -m)
 export HDF5_VERSION="1.12.2"
 export HDF5_DIR="$PROJECT_PATH/cache/hdf5/$HDF5_VERSION-$ARCH"
+# When compiling HDF5, we should use the minimum across all Python versions for a given
+# arch, for versions see for example a more updated version of the following:
+# https://github.com/pypa/cibuildwheel/blob/89a5cfe2721c179f4368a2790669e697759b6644/cibuildwheel/macos.py#L296-L310
+if [[ "$ARCH" == "arm64" ]]; then
+    export MACOSX_DEPLOYMENT_TARGET="11.0"
+else
+    export MACOSX_DEPLOYMENT_TARGET="10.9"
+fi
 source $SCRIPT_DIR/get_hdf5_if_needed.sh
 
 if [[ "$GITHUB_ENV" != "" ]]; then
