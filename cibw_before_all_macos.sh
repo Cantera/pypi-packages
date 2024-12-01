768922aee27049091efec26ff76072e27d133af1
Bryan Weber <bryan.w.weber@gmail.com>
2024-11-30 20:44:58 -0500

    Refactor building dependencies to a common script

--- cibw_before_all_macos.sh
+++ cibw_before_all_macos.sh
@@ -32,28 +32,26 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 set -eo pipefail
+set +x
 
 function setup_github_env {
     if [[ "$GITHUB_ENV" != "" ]]; then
         echo "HDF5_ROOT=${HDF5_DIR}" | tee -a $GITHUB_ENV
         echo "HighFive_ROOT=${HIGHFIVE_DIR}" | tee -a $GITHUB_ENV
-        echo "LD_LIBRARY_PATH=${LD_LIBRARY_PATH}" | tee -a $GITHUB_ENV
         echo "MACOSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET}" | tee -a $GITHUB_ENV
         echo "DYLD_FALLBACK_LIBRARY_PATH=${HDF5_DIR}/lib" | tee -a $GITHUB_ENV
     fi
 }
 
-set +x
-
 if [[ "$1" == "" ]] ; then
     echo "Usage: $0 <PROJECT_PATH>"
     exit 1
 fi
+
 PROJECT_PATH="$1"
 ARCH=$(uname -m)
+GENERATOR="ninja"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-source "${SCRIPT_DIR}/dependencies.sh"
 
 HDF5_DIR="${PROJECT_PATH}/cache/hdf5/${HDF5_VERSION}-${ARCH}"
 HIGHFIVE_DIR="${PROJECT_PATH}/cache/highfive/${HIGHFIVE_VERSION}-${ARCH}"
@@ -81,54 +79,7 @@ fi
 
 brew install ninja cmake
 
-pushd ${RUNNER_TEMP}
-
-curl -fsSLO "https://github.com/HDFGroup/hdf5/releases/download/hdf5_${HDF5_VERSION}/hdf5-${HDF5_VERSION}.tar.gz"
-tar -xzf hdf5-${HDF5_VERSION}.tar.gz
-mkdir -p hdf5-${HDF5_VERSION}/build
-pushd hdf5-${HDF5_VERSION}/build
-
-cmake -G Ninja \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX="${HDF5_DIR}" \
-    -DHDF5_ENABLE_Z_LIB_SUPPORT:BOOL=ON \
-    -DHDF5_ENABLE_SZIP_SUPPORT:BOOL=ON \
-    -DHDF5_BUILD_EXAMPLES:BOOL=OFF \
-    -DHDF5_BUILD_TOOLS:BOOL=OFF \
-    -DBUILD_TESTING:BOOL=OFF \
-    -DHDF5_ALLOW_EXTERNAL_SUPPORT:STRING=TGZ \
-    -DZLIB_PACKAGE_NAME:STRING=zlib \
-    -DZLIB_TGZ_NAME:STRING=zlib-${ZLIB_VERSION}.tar.gz \
-    -DZLIB_TGZ_ORIGPATH:STRING=https://github.com/madler/zlib/releases/download/v${ZLIB_VERSION} \
-    -DZLIB_USE_LOCALCONTENT:BOOL=OFF \
-    -DLIBAEC_PACKAGE_NAME:STRING=libaec \
-    -DLIBAEC_TGZ_NAME:STRING=libaec-${LIBAEC_VERSION}.tar.gz \
-    -DLIBAEC_TGZ_ORIGPATH:STRING=https://github.com/MathisRosenhauer/libaec/releases/download/v${LIBAEC_VERSION} \
-    -DLIBAEC_USE_LOCALCONTENT:BOOL=OFF \
-    -DHDF_PACKAGE_NAMESPACE:STRING=ct_ \
-    ..
-
-ninja install
-popd
-
-curl -fsSLO https://github.com/BlueBrain/HighFive/archive/refs/tags/v${HIGHFIVE_VERSION}.tar.gz
-tar -xzf v${HIGHFIVE_VERSION}.tar.gz
-mkdir -p HighFive-${HIGHFIVE_VERSION}/build
-pushd HighFive-${HIGHFIVE_VERSION}/build
-
-cmake -G Ninja \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DHDF5_ROOT="${HDF5_DIR}" \
-    -DCMAKE_INSTALL_PREFIX="${HIGHFIVE_DIR}" \
-    -DHIGHFIVE_USE_BOOST:BOOL=OFF \
-    -DHIGHFIVE_UNIT_TESTS:BOOL=OFF \
-    -DHIGHFIVE_EXAMPLES:BOOL=OFF \
-    -DHIGHFIVE_BUILD_DOCS:BOOL=OFF \
-    ..
-
-ninja install
-popd
+source "${SCRIPT_DIR}/dependencies.sh"
+source "${SCRIPT_DIR}/build_dependencies.sh"
 
 setup_github_env
-
-set -x
--- cibw_before_all_windows.sh
+++ cibw_before_all_windows.sh
@@ -32,80 +32,38 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 set -eo pipefail
+set +x
+
+function setup_github_env {
+    echo "HDF5_ROOT=$HDF5_DIR" | tee -a $GITHUB_ENV
+    echo "HDF5_LIB_DIR=$HDF5_DIR\bin" | tee -a $GITHUB_ENV
+    echo "HighFive_ROOT=$HIGHFIVE_DIR" | tee -a $GITHUB_ENV
+}
 
 if [[ "$1" == "" ]] ; then
     echo "Usage: $0 <PROJECT_PATH>"
     exit 1
 fi
 PROJECT_PATH="$1"
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-source "${SCRIPT_DIR}/dependencies.sh"
+GENERATOR="Visual Studio 17 2022"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 HDF5_DIR="${PROJECT_PATH}/cache/hdf5/${HDF5_VERSION}"
 HIGHFIVE_DIR="${PROJECT_PATH}/cache/highfive/${HIGHFIVE_VERSION}"
 
-pushd ${RUNNER_TEMP}
+lib_name=hdf5.dll
+inc_name=highfive.hpp
 
-set +x
-
-curl -fsSLO "https://github.com/HDFGroup/hdf5/releases/download/hdf5_${HDF5_VERSION}/hdf5-${HDF5_VERSION}.tar.gz"
-tar -xzf hdf5-${HDF5_VERSION}.tar.gz
-mkdir -p hdf5-${HDF5_VERSION}/build
-pushd hdf5-${HDF5_VERSION}/build
-
-cmake -G "Visual Studio 17 2022" \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX="${HDF5_DIR}" \
-    -DHDF5_ENABLE_Z_LIB_SUPPORT:BOOL=ON \
-    -DHDF5_ENABLE_SZIP_SUPPORT:BOOL=ON \
-    -DHDF5_BUILD_EXAMPLES:BOOL=OFF \
-    -DHDF5_BUILD_TOOLS:BOOL=OFF \
-    -DBUILD_TESTING:BOOL=OFF \
-    -DHDF5_ALLOW_EXTERNAL_SUPPORT:STRING=TGZ \
-    -DZLIB_PACKAGE_NAME:STRING=zlib \
-    -DZLIB_TGZ_NAME:STRING=zlib-${ZLIB_VERSION}.tar.gz \
-    -DZLIB_TGZ_ORIGPATH:STRING=https://github.com/madler/zlib/releases/download/v${ZLIB_VERSION} \
-    -DZLIB_USE_LOCALCONTENT:BOOL=OFF \
-    -DLIBAEC_PACKAGE_NAME:STRING=libaec \
-    -DLIBAEC_TGZ_NAME:STRING=libaec-${LIBAEC_VERSION}.tar.gz \
-    -DLIBAEC_TGZ_ORIGPATH:STRING=https://github.com/MathisRosenhauer/libaec/releases/download/v${LIBAEC_VERSION} \
-    -DLIBAEC_USE_LOCALCONTENT:BOOL=OFF \
-    -DHDF_PACKAGE_NAMESPACE:STRING=ct_ \
-    ..
-
-cmake --build . --target install --config Release
-popd
-
-curl -fsSLO https://github.com/BlueBrain/HighFive/archive/refs/tags/v${HIGHFIVE_VERSION}.tar.gz
-tar -xzf v${HIGHFIVE_VERSION}.tar.gz
-mkdir -p HighFive-${HIGHFIVE_VERSION}/build
-pushd HighFive-${HIGHFIVE_VERSION}/build
-
-cmake -G "Visual Studio 17 2022" \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DHDF5_ROOT="${HDF5_DIR}" \
-    -DCMAKE_INSTALL_PREFIX="${HIGHFIVE_DIR}" \
-    -DHIGHFIVE_USE_BOOST:BOOL=OFF \
-    -DHIGHFIVE_UNIT_TESTS:BOOL=OFF \
-    -DHIGHFIVE_EXAMPLES:BOOL=OFF \
-    -DHIGHFIVE_BUILD_DOCS:BOOL=OFF \
-    ..
-
-cmake --build . --target install --config Release
-popd
-
-find $HDF5_DIR -type f
-find $HIGHFIVE_DIR -type f
-
-if [[ "$GITHUB_ENV" != "" ]] ; then
-    # PATH on windows is special
-    echo "$EXTRA_PATH" | tee -a $GITHUB_PATH
-    echo "CL=$CL" | tee -a $GITHUB_ENV
-    echo "LINK=$LINK" | tee -a $GITHUB_ENV
-    echo "HDF5_ROOT=$HDF5_DIR" | tee -a $GITHUB_ENV
-    echo "HDF5_LIB_DIR=$HDF5_DIR\bin" | tee -a $GITHUB_ENV
-    echo "HighFive_ROOT=$HIGHFIVE_DIR" | tee -a $GITHUB_ENV
+if [ -f ${HDF5_DIR}/bin/${lib_name} ] && [ -f ${HIGHFIVE_DIR}/include/highfive/${inc_name} ]; then
+    echo "using cached build"
+    setup_github_env
+    exit 0
+else
+    echo "building HDF5"
 fi
 
-set -x
+source "${SCRIPT_DIR}/dependencies.sh"
+source "${SCRIPT_IDR}/build_dependencies.sh"
+
+setup_github_env
